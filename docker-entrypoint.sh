216fd2c523c6056519079b0c97207617ba91b9c1
sparklyballs <sparklyballs@gmail.com>
2022-01-21 20:54:38 +0000

    use latest chia style with separate build and service env

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,8 +14,8 @@ cd /skynet-blockchain || exit 1
 skynet init --fix-ssl-permissions
 
 if [[ ${testnet} == 'true' ]]; then
-   echo "configure testnet"
-   skynet configure --testnet true
+  echo "configure testnet"
+  skynet configure --testnet true
 fi
 
 if [[ ${keys} == "persistent" ]]; then
@@ -26,7 +26,7 @@ elif [[ ${keys} == "generate" ]]; then
 elif [[ ${keys} == "copy" ]]; then
   if [[ -z ${ca} ]]; then
     echo "A path to a copy of the farmer peer's ssl/ca required."
-	exit
+    exit
   else
   skynet init -c "${ca}"
   fi
@@ -35,12 +35,14 @@ else
 fi
 
 for p in ${plots_dir//:/ }; do
-    mkdir -p "${p}"
-    if [[ ! $(ls -A "$p") ]]; then
-        echo "Plots directory '${p}' appears to be empty, try mounting a plot directory with the docker -v command"
-    fi
-    skynet plots add -d "${p}"
+  mkdir -p "${p}"
+  if [[ ! $(ls -A "$p") ]]; then
+    echo "Plots directory '${p}' appears to be empty, try mounting a plot directory with the docker -v command"
+  fi
+  skynet plots add -d "${p}"
 done
+
+skynet configure --upnp "${upnp}"
 
 if [[ -n "${log_level}" ]]; then
   skynet configure --log-level "${log_level}"
@@ -54,6 +56,30 @@ if [[ -n "${outbound_peer_count}" ]]; then
   skynet configure --set_outbound-peer-count "${outbound_peer_count}"
 fi
 
-sed -i 's/localhost/127.0.0.1/g' "$CONFIG_ROOT/config/config.yaml"
+if [[ -n ${farmer_address} && -n ${farmer_port} ]]; then
+  skynet configure --set-farmer-peer "${farmer_address}:${farmer_port}"
+fi
+
+sed -i 's/localhost/127.0.0.1/g' "$SKYNET_ROOT/config/config.yaml"
+
+if [[ ${log_to_file} != 'true' ]]; then
+  sed -i 's/log_stdout: false/log_stdout: true/g' "$SKYNET_ROOT/config/config.yaml"
+else
+  sed -i 's/log_stdout: true/log_stdout: false/g' "$SKYNET_ROOT/config/config.yaml"
+fi
+
+# Map deprecated legacy startup options.
+if [[ ${farmer} == "true" ]]; then
+  service="farmer-only"
+elif [[ ${harvester} == "true" ]]; then
+  service="harvester"
+fi
+
+if [[ ${service} == "harvester" ]]; then
+  if [[ -z ${farmer_address} || -z ${farmer_port} || -z ${ca} ]]; then
+    echo "A farmer peer address, port, and ca path are required."
+    exit
+  fi
+fi
 
 exec "$@"
--- docker-start.sh
+++ docker-start.sh
@@ -1,20 +1,15 @@
 #!/usr/bin/env bash
 
 # shellcheck disable=SC2154
-if [[ ${farmer} == 'true' ]]; then
-  skynet start farmer-only
-elif [[ ${harvester} == 'true' ]]; then
-  if [[ -z ${farmer_address} || -z ${farmer_port} || -z ${ca} ]]; then
-    echo "A farmer peer address, port, and ca path are required."
-    exit
-  else
-    skynet configure --set-farmer-peer "${farmer_address}:${farmer_port}"
-    skynet start harvester
-  fi
-else
-  skynet start farmer
+skynet start "${service}"
+
+trap "echo Shutting down ...; skynet stop all -d; exit 0" SIGINT SIGTERM
+
+# shellcheck disable=SC2154
+if [[ ${log_to_file} == 'true' ]]; then
+  # Ensures the log file actually exists, so we can tail successfully
+  touch "$SKYNET_ROOT/log/debug.log"
+  tail -F "$SKYNET_ROOT/log/debug.log" &
 fi
 
-# Ensures the log file actually exists, so we can tail successfully
-touch "$CONFIG_ROOT/log/debug.log"
-tail -f "$CONFIG_ROOT/log/debug.log"
+while true; do sleep 1; done
